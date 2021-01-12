276e0fda7dd72e2323d56a045e91ba44a7d574f7
Nishi <nishi@Nishis-MacBook-Pro.local>
2021-01-12 23:04:24 +0900

    ビルドエラー修正

--- app/lib/gtag.ts
+++ app/lib/gtag.ts
@@ -1,5 +1,11 @@
 export const GA_TRACKING_ID = String(process.env.NEXT_PUBLIC_GA_TRACKING_ID);
 
+declare global {
+  interface Window {
+    gtag: any;
+  }
+}
+
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string): void => {
   window.gtag('config', GA_TRACKING_ID, {
