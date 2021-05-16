1ce33f54f33afe7d85ca50672e5b4f90acf71bea
Nishi <nishi@Nishis-MacBook-Pro.local>
2021-05-16 23:40:06 +0900

    仙台マークワン屋上、長野市街、父母ヶ浜、桜島と鹿児島市内のカメラを追加

--- app/utils/GetCameraInfoList.ts
+++ app/utils/GetCameraInfoList.ts
@@ -2,6 +2,11 @@ import { CameraInfo } from '../interfaces/type';
 
 /** YouTubeライブカメラ情報一覧を取得 */
 const GetCameraInfoList = (): CameraInfo[] => [
+  // 宮城
+  {
+    channelId: 'UCKYimk34K4xqApVYTKg6sow', q: '仙台マークワン屋上', location: { lat: 38.26191822373228, lng: 140.8813481561511 }, videoId: 'Mwj81Enkd8c',
+  },
+
   // 東京
   {
     channelId: 'UCGCZAYq5Xxojl_tSXcVJhiQ', q: '渋谷スクランブル交差点', location: { lat: 35.659707, lng: 139.70046 }, videoId: 'HpdO5Kq3o7Y',
@@ -32,6 +37,11 @@ const GetCameraInfoList = (): CameraInfo[] => [
     channelId: 'UCbn5eHDjwmPC2K9RG8P0i_A', q: '草津温泉「湯畑」湯滝前', location: { lat: 36.62338661406197, lng: 138.59692722865566 }, videoId: 'GzbPVzPnT-U',
   },
 
+  // 長野
+  {
+    channelId: 'UCy9ww22FuUlXd0c6B8INJVQ', q: '長野市街地', location: { lat: 36.65058896053399, lng: 138.18318525644685 }, videoId: 'zUka6hg6wP0',
+  },
+
   // 山梨
   {
     channelId: 'UCiIA2H7KnbK7Wsyhpw1_glw', q: '河口湖駅前', location: { lat: 35.498530043145216, lng: 138.76882169762285 }, videoId: 'tLQOIro0kgc',
@@ -47,9 +57,19 @@ const GetCameraInfoList = (): CameraInfo[] => [
     channelId: 'UCnpKhR0jbJgGyvKr5BaRt9w', q: '明石海峡大橋', location: { lat: 34.633529452359326, lng: 135.0344058782148 }, videoId: 'gLUx48f1wH4',
   },
 
+  // 香川
+  {
+    channelId: 'UCcNpMZYWUnTNePHU0IO38pQ', q: '父母ヶ浜', location: { lat: 34.18942, lng: 133.6487658 }, videoId: 'zWZJnXqoDVg',
+  },
+
   // 愛媛
   {
     channelId: 'UClAtpDytL1hmOUeiJaNrHlQ', q: '道後温泉本館ライブカメラ（本館カメラ）', location: { lat: 33.85218000114081, lng: 132.78605045021052 }, videoId: '4vpIzKSWL7k',
+  },
+
+  // 鹿児島
+  {
+    channelId: 'UCynXCHprmOfyGzGSnqgp6Wg', q: '桜島と鹿児島市内', location: { lat: 31.575231222557242, lng: 130.52634223469576 }, videoId: 'i5MyvNYBCtI',
   },
 ];
 
